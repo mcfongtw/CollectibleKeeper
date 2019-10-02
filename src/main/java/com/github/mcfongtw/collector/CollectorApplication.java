7f7c8d3e48f62e54c20390e310e364ab62577cd2
Michael Fong <michael.fong@bizrate.com>
2019-10-02 10:23:40 -0700

    add scripts tag in welcome.html to bring back bootstrap.js

--- src/main/java/com/github/mcfongtw/collector/CollectorApplication.java
+++ src/main/java/com/github/mcfongtw/collector/CollectorApplication.java
@@ -1,14 +1,9 @@
 package com.github.mcfongtw.collector;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class CollectorApplication {
 
     public static void main(String[] args) {
